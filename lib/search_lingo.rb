56f7354fa78498d7ca3276d861a1f64325a5fd76
John Parker <jparker@urgetopunt.com>
2015-06-02 15:12:33 +0900

    set up search_lingo.rb to load necessary files

--- lib/search_lingo.rb
+++ lib/search_lingo.rb
@@ -1,2 +1,7 @@
 require 'search_lingo/version'
-require 'search_lingo/tokenizer'
+require 'search_lingo/abstract_search'
+
+require 'search_lingo/parsers/date_parser'
+require 'search_lingo/parsers/date_range_parser'
+require 'search_lingo/parsers/gte_date_parser'
+require 'search_lingo/parsers/lte_date_parser'
