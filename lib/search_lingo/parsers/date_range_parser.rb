b6728bcd0e847e872cf8fe5591ad951bef4739fe
John Parker <jparker@urgetopunt.com>
2015-06-01 17:58:25 -0700

    added DateRangeParser

---
+++ lib/search_lingo/parsers/date_range_parser.rb
@@ -0,0 +1,15 @@
+require 'search_lingo/parsers/date_parser'
+
+module SearchLingo
+  module Parsers
+    class DateRangeParser < DateParser
+      def call(token)
+        token.match /\A#@prefix(?<min>#{US_DATE})-(?<max>#{US_DATE})\Z/ do |m|
+          min = parse m[:min]
+          max = parse m[:max], relative_to: min.next_year if min
+          [:where, { table => { column => min..max } }] if min && max
+        end
+      end
+    end
+  end
+end
