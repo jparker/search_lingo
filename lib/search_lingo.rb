3779004d1874e6804e22d4b5bd3b7e8ad02d06b5
John Parker <jparker@urgetopunt.com>
2015-06-08 16:55:57 +0800

    added search_lingo/parsers to load included parsers

--- lib/search_lingo.rb
+++ lib/search_lingo.rb
@@ -1,7 +1,3 @@
 require 'search_lingo/version'
 require 'search_lingo/abstract_search'
-
-require 'search_lingo/parsers/date_parser'
-require 'search_lingo/parsers/date_range_parser'
-require 'search_lingo/parsers/gte_date_parser'
-require 'search_lingo/parsers/lte_date_parser'
+require 'search_lingo/parsers'
---
+++ lib/search_lingo/parsers.rb
@@ -0,0 +1,4 @@
+require 'search_lingo/parsers/date_parser'
+require 'search_lingo/parsers/date_range_parser'
+require 'search_lingo/parsers/gte_date_parser'
+require 'search_lingo/parsers/lte_date_parser'
