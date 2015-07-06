a4b33ba325874d50731babc6ba82c60f0891f29f
John Parker <jparker@urgetopunt.com>
2015-07-06 15:07:05 -0700

    restored development dependency on pry
    I forgot that pry is required in bin/console, and I really want to be
    able to use pry as the console.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,10 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'minitest/autorun'
+require 'pry'
 
 begin
   require 'minitest/focus'
-  require 'pry'
 
   # FYI: minitest-reporters does not work with autotest :(
   # https://github.com/kern/minitest-reporters/issues/102
