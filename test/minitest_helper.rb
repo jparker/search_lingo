38eb20fbc785d12a3ab4e813bf6068ede6a460c9
John Parker <jparker@urgetopunt.com>
2015-07-06 13:07:51 -0700

    removed dev dependencies on pry, minitest-focus, minitest-reporters

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,10 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'minitest/autorun'
-require 'minitest/focus'
-require 'pry'
 
-# FYI: minitest-reporters does not work with autotest :(
-# https://github.com/kern/minitest-reporters/issues/102
-require 'minitest/reporters'
-Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
+begin
+  require 'minitest/focus'
+  require 'pry'
+
+  # FYI: minitest-reporters does not work with autotest :(
+  # https://github.com/kern/minitest-reporters/issues/102
+  require 'minitest/reporters'
+  Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
+rescue LoadError
+end
